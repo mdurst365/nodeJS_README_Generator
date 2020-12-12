ba2566f7847cbc71a0f5f95c379c149177614a4b
mdurst365 <71846329+mdurst365@users.noreply.github.com>
2020-12-11 19:30:43 -0500

    added questions and comments

--- index.js
+++ index.js
@@ -1,5 +1,52 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
+
 // array of questions for user
 const questions = [
+
+//Github User Name
+{
+    type: 'input',
+    message: 'What is your GitHub username?',
+    name: 'username',
+},     
+//Email Address
+{
+    type: 'input',
+    message: 'What is your email address?',
+    name: 'email',
+},    
+//Project Title
+{
+    type: 'input',
+    message: 'What is the title of your project?',
+    name: 'Title',
+}, 
+// Project Description
+{
+    type: 'input',
+    message: 'Please describe your project.',
+    name: 'Description',
+}, 
+// Installation
+{
+    type: 'input',
+    message: 'Please describe installation instructions',
+    name: 'Installation',
+}, 
+// Contribution Guidelines
+{
+    type: 'input',
+    message: 'Please describe any contribution guidelines',
+    name: 'contribution',
+}, 
+// List of licenses to choose from
+{
+    type: 'list',
+    message: 'What license will you use for your project?',
+    name: 'license',
+},   
 
 ];
 
